bcd990a193225aff9b7b552e79c69e0be368d881
nasouh94 <nasouh@live.com>
2019-10-08 21:36:31 +0000

    created a createtweetelement function and a render tweets, that for now creats a tweet from the hardcoded database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+function renderTweets(tweets) {
+  for(let tweet of tweets) {
+    console.log(createTweetElement(tweet))
+    $("tweet-container").prepend(createTweetElement(tweet))
+  }
+}
+
+
+function createTweetElement(tweet) {
+
+  let $tweet = $("<article>").addClass("tweet-container");
+
+  const markup = `
+  <article class ="tweet-container">
+                <img class= "profile-picture" src="${tweet.user.avatars}"> 
+              <p class="username">${tweet.user.name}</p>
+              <p class="handler">${tweet.user.handle}</p>
+              <p class="tweet-content">${tweet.content.text}</p>
+              <p class="date">${tweet.created_at}</p>
+              </article>
+  `;
+ $tweet = $tweet.append(markup);
+
+  return $tweet;
+
+}
+console.log(createTweetElement(tweet))
+
+
+
+renderTweets(data);
