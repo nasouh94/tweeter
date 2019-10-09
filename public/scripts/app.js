6f72280079bd06f2f354d302f052721c16e23355
nasouh94 <nasouh@live.com>
2019-10-09 19:42:36 +0000

    added a loadtweet function and post function along with a validate input function that checks if the input is valid and sends an error message accordingly

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,59 +4,76 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 function renderTweets(tweets) {
-  for(let tweet of tweets) {
-    console.log(createTweetElement(tweet))
-    $("tweet-container").prepend(createTweetElement(tweet))
+  for (let tweet of tweets) {
+    // console.log(createTweetElement(tweet))
+    $(".tweet-container").prepend(createTweetElement(tweet))
   }
 }
 
 
 function createTweetElement(tweet) {
-
-  // let $tweet = $("<article>").addClass("tweet-container");
-
-  const $tweet = `
-  <article class ="tweet-container">
-                <img class= "profile-picture" src="${tweet.user.avatars}"> 
-              <p class="username">${tweet.user.name}</p>
-              <p class="handler">${tweet.user.handle}</p>
-              <p class="tweet-content">${tweet.content.text}</p>
-              <p class="date">${tweet.created_at}</p>
-              </article>
-  `;
-//  $tweet = $tweet.append(markup);
+  // let $tweet = $("<article>").addClass("tweet");
+  const $tweet = ` 
+<article class ="tweet">
+              <header>
+                  <img class= "profile-picture" src="${tweet.user.avatars}"> 
+                  <p class="username">${tweet.user.name}</p>
+                  <p class="handler">${tweet.user.handle}</p>
+              </header>
+              <body>
+                <p class="tweet-content">${tweet.content.text}</p>
+              </body>
+              <footer>
+                <p class="date">${tweet.created_at}</p>
+              </footer>
+            </article>`
+  //  $tweet = $tweet.append(markup);
 
   return $tweet;
 
 }
 
 
+function validateInput(textInput) {
+  if (textInput === "") {
+    return "tweet is empty"
+  } else if (textInput.length > 140) {
+    return "tweet is over 140 "
+  } else {
+    return false;
+  }
+}
+
+$(function () {
+  const $button = $("#tweet-button");
+  $button.on("click", function (event) {
+    event.preventDefault();
+    let textInput = $("#textarea").serialize();
+    let text = textInput.split("=")[1]
+    let error = validateInput(text)
+    if (error) {
+      alert(error);
+    } else {
+      $.ajax("/tweets/", { method: "POST", data: textInput })
+        .done(function () {
+        }).error(function () {
+          console.log("post tweet error")
+        })
+    }
+  })
+})
 
 
-renderTweets(data);
+function loadTweet() {
+  $.ajax("/tweets/", { method: "GET" })
+    .done(function (data) {
+      renderTweets(data)
+    })
+}
+
+$(document).ready(function () {
+  loadTweet()
+})
+
