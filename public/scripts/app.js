476488ec1df08fc2144dcf64c696d7676f606a6d
nasouh94 <nasouh@live.com>
2019-10-16 18:38:23 +0000

    took out unused code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -102,17 +102,6 @@ function toggleBox() {
   });
 }
 
-//a function that gets us the time and date
-function formatAMPM() {
-  let d = new Date(),
-    minutes = d.getMinutes().toString().length === 1 ? '0' + d.getMinutes() : d.getMinutes(),
-    hours = d.getHours().toString().length === 1 ? '0' + d.getHours() : d.getHours(),
-    ampm = d.getHours() >= 12 ? 'pm' : 'am',
-    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-    days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-  return days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ampm;
-};
-
 
 //hides the button at the top corner when scrolling
 function hideButton() {
